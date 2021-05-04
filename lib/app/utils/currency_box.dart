8e2b86d05ee094dc0b333ec28871518f3d6298fc
Leonardo Souza <leob22.ls@gmail.com>
2021-05-04 15:56:37 -0300

    Completed flow of currency conversion
    finished

--- lib/app/utils/currency_box.dart
+++ lib/app/utils/currency_box.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:project_x/app/models/currency_model.dart';
 
 class CurrencyBox extends StatelessWidget {
+  final List<CurrencyModel> currencies;
+  final TextEditingController controller;
+  final void Function(CurrencyModel? model)? onChanged;
+  final CurrencyModel selectedItem;
+
+  const CurrencyBox(
+      {Key? key,
+      required this.currencies,
+      required this.controller,
+      required this.onChanged,
+      required this.selectedItem})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -10,7 +23,19 @@ class CurrencyBox extends StatelessWidget {
           flex: 1,
           child: SizedBox(
             height: 56,
-            child: DropdownWidget(),
+            child: DropdownButton<CurrencyModel>(
+              value: selectedItem,
+              iconEnabledColor: Colors.green,
+              isExpanded: true,
+              underline: Container(
+                height: 1,
+                color: Colors.green,
+              ),
+              items: currencies
+                  .map((e) => DropdownMenuItem(value: e, child: Text(e.name)))
+                  .toList(),
+              onChanged: onChanged,
+            ),
           ),
         ),
         Container(
@@ -19,6 +44,7 @@ class CurrencyBox extends StatelessWidget {
         Expanded(
           flex: 2,
           child: TextField(
+            controller: controller,
             decoration: InputDecoration(
               enabledBorder: UnderlineInputBorder(
                 borderSide: BorderSide(color: Colors.blue),
@@ -27,45 +53,6 @@ class CurrencyBox extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-/// This is the stateful widget that the main application instantiates.
-class DropdownWidget extends StatefulWidget {
-  const DropdownWidget({Key? key}) : super(key: key);
-
-  @override
-  _DropdownWidgetState createState() => _DropdownWidgetState();
-}
-
-/// This is the private State class that goes with DropdownWidget.
-class _DropdownWidgetState extends State<DropdownWidget> {
-  String dropdownValue = 'Real R\$';
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: dropdownValue,
-      isExpanded: true,
-      elevation: 16,
-      style: const TextStyle(color: Colors.green),
-      underline: Container(
-        height: 2,
-        color: Colors.green,
-      ),
-      onChanged: (String? newValue) {
-        setState(() {
-          dropdownValue = newValue!;
-        });
-      },
-      items: <String>['Real R\$', 'Dollar\$', 'Euro €', 'Bitcoin ฿']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
     );
   }
 }
--- lib/app/views/currency_view.dart
+++ lib/app/views/currency_view.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:project_x/app/controllers/currency_controller.dart';
+import 'package:project_x/app/models/currency_model.dart';
 import 'package:project_x/app/utils/currency_box.dart';
 
-// ignore: must_be_immutable
-class CurrencyView extends StatelessWidget {
+class CurrencyView extends StatefulWidget {
+  @override
+  _CurrencyViewState createState() => _CurrencyViewState();
+}
+
+class _CurrencyViewState extends State<CurrencyView> {
   final TextEditingController toText = TextEditingController();
   final TextEditingController fromText = TextEditingController();
   CurrencyController currencyController =
-      new CurrencyController(TextEditingController(), TextEditingController());
+      CurrencyController(TextEditingController(), TextEditingController());
+  final CurrencyModel selectedItem =
+      CurrencyModel(name: '', real: 0, dollar: 0, euro: 0, bitcoin: 0);
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,24 +38,44 @@ class CurrencyView extends StatelessWidget {
                 children: [
                   Icon(
                     Icons.monetization_on,
-                    color: Colors.blue[900],
+                    color: Colors.green,
                     size: 100,
                   ),
                   SizedBox(
                     height: 50,
                   ),
-                  CurrencyBox(),
+                  CurrencyBox(
+                    selectedItem: currencyController.toCurrency,
+                    currencies: currencyController.currencies,
+                    onChanged: (model) {
+                      setState(() {
+                        currencyController.toCurrency = model!;
+                      });
+                    },
+                    controller: currencyController.toText,
+                  ),
                   SizedBox(
                     height: 10,
                   ),
-                  CurrencyBox(),
+                  CurrencyBox(
+                    selectedItem: currencyController.fromCurrency,
+                    currencies: currencyController.currencies,
+                    onChanged: (model) {
+                      setState(() {
+                        currencyController.fromCurrency = model!;
+                      });
+                    },
+                    controller: currencyController.fromText,
+                  ),
                   SizedBox(
                     height: 30,
                   ),
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                        primary: Colors.blue[900], onPrimary: Colors.white),
-                    onPressed: () {},
+                        primary: Colors.green, onPrimary: Colors.white),
+                    onPressed: () {
+                      currencyController.convert();
+                    },
                     child: Text(
                       'CONVETER',
                     ),
